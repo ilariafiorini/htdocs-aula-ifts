6e8d0ed5caab96a98eead550227c76a4575f528c
ilaria <ilaria@ilariafiorini.it>
2023-01-16 12:31:39 +0100

    ancora roba

--- ing-soft/esempio1/index.php
+++ ing-soft/esempio1/index.php
@@ -8,6 +8,19 @@
 </head>
 <body>
     <?php
+    /** 
+     * calcola il fattoriale di un numero
+     * @param integer $valore: valore del quale calcolare il fattoriale
+     * @return integer
+     */
+
+    function fattoriale($valore){
+        if ($valore == 1){
+            return 1;
+        } else {
+            return fattoriale($valore - 1) * $valore;
+        }
+    }
 
     // $pippo = 0;
     // echo $pippo."<br>";
@@ -16,6 +29,9 @@
     // echo $pippo."<br>";
     
         echo 'ciao mondo!';
+        for($i=0;$i < 10; $i++){
+        echo $i;
+        }
 
     ?>
 </body>
